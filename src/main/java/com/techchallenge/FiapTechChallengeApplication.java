66f6c73f653e838b90cacd8a295ef789226d2ccd
Samuel Leite <samuel.lemeleite@gmail.com>
2025-07-25 20:34:57 -0300

    Inclusão dos outros componentes

--- src/main/java/com/techchallenge/FiapTechChallengeApplication.java
+++ src/main/java/com/techchallenge/FiapTechChallengeApplication.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class FiapTechChallengeApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FiapTechChallengeApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FiapTechChallengeApplication.class, args);
+    }
 }
